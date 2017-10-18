c30b0586c6c5ab773c8bd6ab878047d8fbfadf30
Brian Ma <brina41005@gmail.com>
2017-10-19 00:06:40 +0800

    format

--- utStruct.h
+++ utStruct.h
@@ -4,13 +4,12 @@
 #include <vector>
 #include "include/atom.h"
 #include "include/number.h"
-#include "include/variable.h"
 #include "include/struct.h"
+#include "include/variable.h"
 
 using std::vector;
 
-TEST(Struct, hobby)
-{
+TEST(Struct, hobby) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
@@ -20,17 +19,15 @@ TEST(Struct, hobby)
     ASSERT_EQ("chaseMouse", hobby.args(1)->symbol());
 }
 
-TEST(Struct, symbol)
-{
+TEST(Struct, symbol) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
     Struct hobby(Atom("hobby"), v);
-    ASSERT_EQ("hobby(tom, chaseMouse)",hobby.symbol());
+    ASSERT_EQ("hobby(tom, chaseMouse)", hobby.symbol());
 }
 
-TEST(Struct, match1)
-{
+TEST(Struct, match1) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
@@ -39,7 +36,7 @@ TEST(Struct, match1)
     EXPECT_FALSE(hobby.match(hobby2));
 }
 
-TEST(Struct, match2){
+TEST(Struct, match2) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
@@ -49,18 +46,17 @@ TEST(Struct, match2){
     EXPECT_FALSE(hobby.match(hobby2));
 }
 
-TEST(Struct, match3){
+TEST(Struct, match3) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
     Struct hobby(Atom("hobby"), v);
-    vector<Term *> v1 = { &chaseMouse,&tom};
+    vector<Term *> v1 = {&chaseMouse, &tom};
     Struct hobby2(Atom("hobby"), v1);
     EXPECT_FALSE(hobby.match(hobby2));
 }
 
-TEST(Struct, match4)
-{
+TEST(Struct, match4) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
@@ -69,8 +65,7 @@ TEST(Struct, match4)
     EXPECT_TRUE(hobby.match(hobby2));
 }
 
-TEST(Struct, match5)
-{
+TEST(Struct, match5) {
     Atom tom("tom");
     Atom chaseMouse("chaseMouse");
     vector<Term *> v = {&tom, &chaseMouse};
@@ -81,8 +76,7 @@ TEST(Struct, match5)
 // When Struct s contains a Variable X
 // Then #symbol() should return "s(X)"
 // and #value() should also return "s(X)"
-TEST(Struct, var)
-{
+TEST(Struct, var) {
     Variable X("X");
     vector<Term *> v = {&X};
     Struct s(Atom("s"), v);
@@ -93,8 +87,7 @@ TEST(Struct, var)
 // When Variable X matches an Atom "tom"
 // Then #symbol() should return "s(X)"
 // and #value() should also return "s(tom)"
-TEST(Struct, var_match_atom)
-{
+TEST(Struct, var_match_atom) {
     Variable X("X");
     vector<Term *> v = {&X};
     Struct s(Atom("s"), v);
@@ -103,7 +96,6 @@ TEST(Struct, var_match_atom)
     ASSERT_TRUE(X.match(a1));
     ASSERT_EQ("s(X)", s.symbol());
     ASSERT_EQ("s(tom)", s.value());
-
 }
 
 // Given there are Struct s1 and Struct s2
@@ -111,12 +103,11 @@ TEST(Struct, var_match_atom)
 // And Struct s2 contains a Variable X
 // Then #symbol() of s1 should return "s1(s2(X))"
 // and #value() of s1 should also return "s1(s2(X))"
-TEST(Struct, nested_struct1)
-{
+TEST(Struct, nested_struct1) {
     Variable X("X");
-    vector<Term*> v1 = {&X};
+    vector<Term *> v1 = {&X};
     Struct s2(Atom("s2"), v1);
-    vector<Term*> v2 = {&s2};
+    vector<Term *> v2 = {&s2};
     Struct s1(Atom("s1"), v2);
     ASSERT_EQ("s1(s2(X))", s1.symbol());
     ASSERT_EQ("s1(s2(X))", s1.value());
@@ -127,12 +118,11 @@ TEST(Struct, nested_struct1)
 // When Variable X matches an Atom "tom"
 // Then #symbol() of s1 should return "s1(s2(X))"
 // and #value() of s1 should return "s1(s2(tom))"
-TEST(Struct, nested_struct2)
-{
+TEST(Struct, nested_struct2) {
     Variable X("X");
-    vector<Term*> v1 = {&X};
+    vector<Term *> v1 = {&X};
     Struct s2(Atom("s2"), v1);
-    vector<Term*> v2 = {&s2};
+    vector<Term *> v2 = {&s2};
     Struct s1(Atom("s1"), v2);
     Atom a1("tom");
     ASSERT_TRUE(X.match(a1));
@@ -145,12 +135,11 @@ TEST(Struct, nested_struct2)
 // When Variable X matches an Number pi: 3.14
 // Then #symbol() of s1 should return "s1(s2(X))"
 // and #value() of s1 should return "s1(s2(3.14))"
-TEST(Struct, nested_struct3)
-{
+TEST(Struct, nested_struct3) {
     Variable X("X");
-    vector<Term*> v1 = {&X};
+    vector<Term *> v1 = {&X};
     Struct s2(Atom("s2"), v1);
-    vector<Term*> v2 = {&s2};
+    vector<Term *> v2 = {&s2};
     Struct s1(Atom("s1"), v2);
     Number n1(3.14);
     ASSERT_TRUE(X.match(n1));
@@ -164,14 +153,13 @@ TEST(Struct, nested_struct3)
 // And Variable X matches Atom "kent_beck"
 // Then #symbol() of s1 should return "s1(s2(Y), X)"
 // and #value() of s1 should return "s1(s2(kent_beck), kent_beck)"
-TEST(Struct, nested_struct_and_multiVariable)
-{
+TEST(Struct, nested_struct_and_multiVariable) {
     Variable X("X"), Y("Y");
 
-    vector<Term*> v1 = {&Y};
+    vector<Term *> v1 = {&Y};
     Struct s2(Atom("s2"), v1);
 
-    vector<Term*> v2 = {&s2, &X};
+    vector<Term *> v2 = {&s2, &X};
     Struct s1(Atom("s1"), v2);
 
     ASSERT_TRUE(X.match(Y));
--- utVariable.h
+++ utVariable.h
@@ -1,105 +1,105 @@
 #ifndef UTVARIABLE_H
 #define UTVARIABLE_H
 
-#include "include/variable.h"
+#include <iostream>
 #include "include/atom.h"
 #include "include/number.h"
 #include "include/struct.h"
-#include <iostream>
+#include "include/variable.h"
 
-TEST(Variable, constructor){
-	Variable X("X");
-	ASSERT_EQ("X", X.symbol());
+TEST(Variable, constructor) {
+    Variable X("X");
+    ASSERT_EQ("X", X.symbol());
 }
 
-TEST(Variable , matching){
-	Atom tom("tom");
-	Variable X("X");
-	X.match(tom);
-	ASSERT_EQ( "tom", X.value());
+TEST(Variable, matching) {
+    Atom tom("tom");
+    Variable X("X");
+    X.match(tom);
+    ASSERT_EQ("tom", X.value());
 }
 
-TEST (Variable , haveValue){
-	Atom tom ("tom");
-	Atom jerry ("jerry");
-	Variable X("X");
-	ASSERT_TRUE(X.match(tom));
-	ASSERT_FALSE(X.match(jerry));
+TEST(Variable, haveValue) {
+    Atom tom("tom");
+    Atom jerry("jerry");
+    Variable X("X");
+    ASSERT_TRUE(X.match(tom));
+    ASSERT_FALSE(X.match(jerry));
 }
 
 // ?- X=2.7182.
 // X=2.7182
-TEST(Variable , numE_to_varX){
-	Variable v1("X");
-	Number n1(2.7182);
-	ASSERT_TRUE(v1.match(n1));
-	ASSERT_EQ("2.7182", n1.value());
+TEST(Variable, numE_to_varX) {
+    Variable v1("X");
+    Number n1(2.7182);
+    ASSERT_TRUE(v1.match(n1));
+    ASSERT_EQ("2.7182", n1.value());
 }
 
 // ?- X=Y, X=1.
 // Y=1
-TEST (Variable, varY_to_varX_and_num1_to_varX) {
-	Variable v1("X"), v2("Y");
-	Number n1(1);
-	ASSERT_TRUE(v1.match(v2));
-	ASSERT_TRUE(v1.match(n1));
-	ASSERT_EQ("1", v2.value());
+TEST(Variable, varY_to_varX_and_num1_to_varX) {
+    Variable v1("X"), v2("Y");
+    Number n1(1);
+    ASSERT_TRUE(v1.match(v2));
+    ASSERT_TRUE(v1.match(n1));
+    ASSERT_EQ("1", v2.value());
 }
-  
+
 // ?- X=Y, Y=1.
 // X=1
-TEST (Variable, varY_to_varX_and_num1_to_varY) {
-	Variable v1("X"), v2("Y");
-	Number n1(1);
-	ASSERT_TRUE(v1.match(v2));
-	ASSERT_TRUE(v2.match(n1));
-	ASSERT_EQ("1", v1.value());
+TEST(Variable, varY_to_varX_and_num1_to_varY) {
+    Variable v1("X"), v2("Y");
+    Number n1(1);
+    ASSERT_TRUE(v1.match(v2));
+    ASSERT_TRUE(v2.match(n1));
+    ASSERT_EQ("1", v1.value());
 }
 
 // ?- X=X, X=1.
 // X=1
-TEST (Variable, varX_match_varX_and_num1_to_varX) {
-	Variable v1("X");
-	Number n1(1);
-	ASSERT_TRUE(v1.match(v1));
-	ASSERT_TRUE(v1.match(n1));
-	ASSERT_EQ("1", v1.value());
+TEST(Variable, varX_match_varX_and_num1_to_varX) {
+    Variable v1("X");
+    Number n1(1);
+    ASSERT_TRUE(v1.match(v1));
+    ASSERT_TRUE(v1.match(n1));
+    ASSERT_EQ("1", v1.value());
 }
 
 // ?- Y=1, X=Y.
 // X=1
-TEST (Variable, num1_to_varY_and_varX_match_varY) {
-	Variable v1("X"), v2("Y");
-	Number n1(1);
-	ASSERT_TRUE(v2.match(n1));
-	ASSERT_TRUE(v1.match(v2));
-	ASSERT_EQ("1", v1.value());
+TEST(Variable, num1_to_varY_and_varX_match_varY) {
+    Variable v1("X"), v2("Y");
+    Number n1(1);
+    ASSERT_TRUE(v2.match(n1));
+    ASSERT_TRUE(v1.match(v2));
+    ASSERT_EQ("1", v1.value());
 }
 
 // ?- X=Y, Y=Z, Z=1
 // X=1, Y=1, Z=1
-TEST (Variable, num1_to_varZ_to_varY_to_varX) {
-	Variable v1("X"), v2("Y"), v3("Z");
-	Number n1(1);
-	ASSERT_TRUE(v1.match(v2));
-	ASSERT_TRUE(v2.match(v3));
-	ASSERT_TRUE(v3.match(n1));
-	ASSERT_EQ("1", v1.value());
-	ASSERT_EQ("1", v2.value());
-	ASSERT_EQ("1", v3.value());
+TEST(Variable, num1_to_varZ_to_varY_to_varX) {
+    Variable v1("X"), v2("Y"), v3("Z");
+    Number n1(1);
+    ASSERT_TRUE(v1.match(v2));
+    ASSERT_TRUE(v2.match(v3));
+    ASSERT_TRUE(v3.match(n1));
+    ASSERT_EQ("1", v1.value());
+    ASSERT_EQ("1", v2.value());
+    ASSERT_EQ("1", v3.value());
 }
 
 // ?- X=Y, X=Z, Z=1
 // X=1, Y=1, Z=1
-TEST (Variable, num1_to_varZ_to_varX_and_varY_to_varX) {
-	Variable v1("X"), v2("Y"), v3("Z");
-	Number n1(1);
-	ASSERT_TRUE(v1.match(v2));
-	ASSERT_TRUE(v1.match(v3));
-	ASSERT_TRUE(v3.match(n1));
-	ASSERT_EQ("1", v1.value());
-	ASSERT_EQ("1", v2.value());
-	ASSERT_EQ("1", v3.value());
+TEST(Variable, num1_to_varZ_to_varX_and_varY_to_varX) {
+    Variable v1("X"), v2("Y"), v3("Z");
+    Number n1(1);
+    ASSERT_TRUE(v1.match(v2));
+    ASSERT_TRUE(v1.match(v3));
+    ASSERT_TRUE(v3.match(n1));
+    ASSERT_EQ("1", v1.value());
+    ASSERT_EQ("1", v2.value());
+    ASSERT_EQ("1", v3.value());
 }
 
 // Give there is a Struct s contains Variable X
@@ -107,14 +107,14 @@ TEST (Variable, num1_to_varZ_to_varX_and_varY_to_varX) {
 // When Y matches Struct s
 // Then #symbol() of Y should return "Y"
 // And #value() of Y should return "s(X)"
-TEST (Variable, Struct1) {
-	Variable X("X"), Y("Y");
-	vector<Term *> v = {&X};
-	Struct s(Atom("s"), v);
-	ASSERT_EQ("s(X)", s.symbol());
-	ASSERT_TRUE(Y.match(s));
-	ASSERT_EQ("Y", Y.symbol());
-	ASSERT_EQ("s(X)", Y.value());
+TEST(Variable, Struct1) {
+    Variable X("X"), Y("Y");
+    vector<Term *> v = {&X};
+    Struct s(Atom("s"), v);
+    ASSERT_EQ("s(X)", s.symbol());
+    ASSERT_TRUE(Y.match(s));
+    ASSERT_EQ("Y", Y.symbol());
+    ASSERT_EQ("s(X)", Y.value());
 }
 
 // Give there is a Struct s contains Variable X
@@ -123,16 +123,16 @@ TEST (Variable, Struct1) {
 // And X matches Atom "teddy"
 // Then #symbol() of Y should return "Y"
 // And #value() of Y should return "s(teddy)"
-TEST (Variable, Struct2) {
-	Variable X("X"), Y("Y");
-	vector<Term *> v = {&X};
-	Struct s(Atom("s"), v);
-	ASSERT_EQ("s(X)", s.symbol());
-	ASSERT_TRUE(Y.match(s));
-	Atom a1("teddy");
-	ASSERT_TRUE(X.match(a1));
-	ASSERT_EQ("Y", Y.symbol());
-	ASSERT_EQ("s(teddy)", Y.value());
+TEST(Variable, Struct2) {
+    Variable X("X"), Y("Y");
+    vector<Term *> v = {&X};
+    Struct s(Atom("s"), v);
+    ASSERT_EQ("s(X)", s.symbol());
+    ASSERT_TRUE(Y.match(s));
+    Atom a1("teddy");
+    ASSERT_TRUE(X.match(a1));
+    ASSERT_EQ("Y", Y.symbol());
+    ASSERT_EQ("s(teddy)", Y.value());
 }
 
 #endif
