a3d034427073770859b56c3f97f0aacfb8ccb9b6
Brian Ma <brina41005@gmail.com>
2017-10-18 23:36:46 +0800

    format

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <gtest/gtest.h>
-#include "../utest/utTerm.h"
-#include "../utVariable.h"
 #include "../utStruct.h"
+#include "../utVariable.h"
+#include "../utest/utTerm.h"
 
-int main(int argc , char **argv)
-{
-    testing :: InitGoogleTest(&argc , argv);
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
