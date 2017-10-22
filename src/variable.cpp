3c43fcf054a0fd65a33f9b9e5bcdb7b88af63678
Brian Ma <brian41005@gmail.com>
2017-10-22 21:32:37 +0800

    refactor

--- src/variable.cpp
+++ src/variable.cpp
@@ -9,14 +9,14 @@ string Variable::value(vector<Term*> record) {
     if (_value){
         vector<Term*>::iterator index = find(record.begin(), record.end(), this);
         if (index != record.end() && index != record.end()-1 && record.size() > 1){
-            if (record[record.size()-2]->getVariable()) {
+
+            if (record[record.size()-2]->getVariable()) 
                 result = record[record.size()-2]->symbol(); 
-            }
             else
                 result = _symbol;
-        }else {
+
+        }else 
             result = _value->value(record);
-        }
     }
     else
         result = _symbol;
@@ -27,17 +27,16 @@ string Variable::value(vector<Term*> record) {
 string Variable::symbol() const { return _symbol; }
 
 bool Variable::match(Term& term) {
-    if (_value == NULL) {
+    if (_value) {
+        if (term.symbol() == _value->symbol() && &term == _value) return true;
+        return _value->match(term);
+    } 
+    else{
         if (Variable* v = term.getVariable()) {
             if (v->symbol() != symbol() && &term != this) _value = &term;
-
         } else
             _value = &term;
         return true;
-    } 
-    else{
-        if (term.symbol() == _value->symbol() && &term == _value) return true;
-        return _value->match(term);
     }
         
 }
