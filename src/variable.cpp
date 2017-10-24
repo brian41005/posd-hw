49db89532d296a9223fa3b3a7805aba33824c1ca
Brian Ma <brian41005@gmail.com>
2017-10-24 15:33:37 +0800

    refactor  variable match

--- src/variable.cpp
+++ src/variable.cpp
@@ -7,17 +7,16 @@ string Variable::value() const { return _value ? _value->value() : symbol(); }
 string Variable::symbol() const { return _symbol; }
 
 bool Variable::match(Term& term) {
-    if (_value == NULL) {
-        if (Variable* v = term.getVariable()) {
-            if (v->symbol() != symbol() && &term != this) _value = &term;
-        }
-        else
-            _value = &term;
-        return true;
-    } 
-    else{
-        if (term.symbol() == _value->symbol() && &term == _value) return true;
-        return _value->match(term);
+    if (Variable* v = term.getVariable()) {
+        if (term.symbol() == symbol() || &term == this) return true;
+        if (v->_value == this) return true;
     }
-        
+    if (_value) {
+        return (term.symbol() == _value->symbol() || &term == _value)
+                   ? true
+                   : _value->match(term);
+    } else {
+        _value = &term;
+        return true;
+    }
 }
