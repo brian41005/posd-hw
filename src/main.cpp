1f6cc4d2d0a6328403f9910878d77cd0050e470f
Brian Ma <brian41005@gmail.com>
2017-11-09 14:31:18 +0800

    add ut scanner

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "../utest/utStruct.h"
 #include "../utest/utTerm.h"
 #include "../utest/utVariable.h"
+#include "../utest/utScanner.h"
 #include "../utest/utParser.h"
 
 int main(int argc, char **argv) {
